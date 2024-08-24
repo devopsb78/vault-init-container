4333da101b23abd56c79accfa592873d7770b13f
RAMBABU <chinnak7416@gmail.com>
2024-08-24 14:43:09 +0530

    data retrieving from vault

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 vault login $VAULT_TOKEN &>/dev/null
+mkdir /data
 vault  kv get $SECRET_NAME  | sed -n -e '/= Data =/,$ p' | grep -Ev '= Data =|^Key|^--' | awk '{print "export "$1"="$2""}' >/data/secrets
 
